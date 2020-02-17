54d73354fe3b32e00e26dd98e4b7c39f34ad3ac5
Urs Enzler <urs.enzler@gmail.com>
2020-02-17 17:46:31 +0100

    now the getName code is executed only once (lazyly)

--- AsyncWorkflowsPlayground/Program.fs
+++ AsyncWorkflowsPlayground/Program.fs
@@ -1,21 +1,23 @@
-﻿open System
+﻿open AsyncLazy
+open System
 
 let (|Over10|UpTo10|) id = if id > 10 then Over10 else UpTo10
 
 let getName (id : int) =
-    async {
-        Console.WriteLine(@"""GetName"" gets executed")
+    AsyncLazy(
+        async {
+            Console.WriteLine(@"""GetName"" gets executed")
 
-        do! Async.Sleep 500
+            do! Async.Sleep 500
 
-        return "Hans Maulwurf (id: " + id.ToString() + ")"
-    }
+            return "Hans Maulwurf (id: " + id.ToString() + ")"
+        })
 
-let printNameOver10 id (name : Async<string>) =
+let printNameOver10 id (name : AsyncLazy<string>) =
     async {
         match id with
         | Over10 ->
-            let! value = name
+            let! value = name.Force()
             Console.WriteLine(value)
         | UpTo10 ->
             Console.WriteLine("under 10")
@@ -23,7 +25,10 @@ let printNameOver10 id (name : Async<string>) =
 
 [<EntryPoint>]
 let main argv =
-    let id = argv.[0] |> int
+    let id = 20 // argv.[0] |> int
     let name = getName id
+
+    // let n = name.Force() |> Async.RunSynchronously    comment in to show that getName is executed only once
+
     printNameOver10 id name |> Async.RunSynchronously
     0
