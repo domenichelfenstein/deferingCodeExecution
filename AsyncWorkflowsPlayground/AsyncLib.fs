6c262b451df7d27befb73f2a12415bb0da8e1887
Domenic Helfenstein <dom.helfenstein@gmail.com>
2020-03-07 14:19:37 +0100

    using replay function from Lev Gorodinskis Blog

---
+++ AsyncWorkflowsPlayground/AsyncLib.fs
@@ -0,0 +1,17 @@
+module AsyncLib
+    open System.Threading
+    open System.Threading.Tasks
+
+    // https://medium.com/jettech/f-async-guide-eb3c8a2d180a
+    let replay a: Async<'a> =
+          let tcs = TaskCompletionSource<'a>()
+          let state = ref 0
+          async {
+           if (Interlocked.CompareExchange(state, 1, 0) = 0) then
+             Async.StartWithContinuations(
+               a, 
+               tcs.SetResult, 
+               tcs.SetException, 
+               (fun _ -> tcs.SetCanceled()))
+           return! tcs.Task |> Async.AwaitTask }
+
--- AsyncWorkflowsPlayground/Program.fs
+++ AsyncWorkflowsPlayground/Program.fs
@@ -1,4 +1,5 @@
-﻿open System
+﻿open AsyncLib
+open System
 
 let (|Over10|UpTo10|) id = if id > 10 then Over10 else UpTo10
 
@@ -11,34 +12,23 @@ let getName (id : int) =
         return "Hans Maulwurf (id: " + id.ToString() + ")"
     }
 
-let printNameOver10 id (name : Lazy<Async<string>>) =
+let printNameOver10 id (name : Async<string>) =
     async {
         match id with
             | Over10 ->
-                let! value = name.Force()
+                let! value = name
                 Console.WriteLine(value)
             | UpTo10 ->
                 Console.WriteLine("under 10")
     }
-    
-let replay task =
-    let mutable memorizedValue = null
-    match memorizedValue with
-        | null ->
-            async {
-                let! result = task
-                memorizedValue <- result
-                return result
-            }
-        | _ -> async { return memorizedValue }
 
 [<EntryPoint>]
 let main argv =
     async {
         let id = 20
-        let name = lazy(getName id |> replay)
+        let name = getName id |> replay
 
-        let! n = name.Force()    // comment in to show that getName is executed only once
+//        let! n = name.Force()    // comment in to show that getName is executed only once
 
         do! printNameOver10 id name
     } |> Async.RunSynchronously
